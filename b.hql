92e20f3a3655b421a2a0410c1c89f4fb8fe1d35f
Matthew Molloy <whamtet@gmail.com>
2022-05-01 19:03:33 +0900

    expand stars

--- b.hql
+++ b.hql
@@ -1,8 +1,18 @@
+CREATE TABLE side_table1 (
+  side_col1 STRING
+);
+CREATE TABLE side_table2 (
+  side_col2 STRING
+);
+CREATE TABLE side_table3 (
+  side_col3 STRING
+);
+
 SELECT
 table1.col1 as alias1,
 col1,
-side_table1.*,
-max(table1.col1)
+*,
+count(*) as count
 FROM
 (SELECT table2.col2 as col1
   FROM
@@ -10,3 +20,4 @@ FROM
   JOIN side_table3 ON (table2.id = side_table3.id)) table1
   JOIN side_table1 ON (table1.id = side_table1.id)
   JOIN side_table2 side_alias ON (table1.id = side_table2.id)
+WHERE hi = there;
--- src/inspect_hql/extract.clj
+++ src/inspect_hql/extract.clj
@@ -9,6 +9,9 @@
   (into {}
         (for [[k v] m :when v]
           [k v])))
+(defn zipmap-by [f1 f2 s] (zipmap (map f1 s) (map f2 s)))
+(defn value-map [f m]
+  (->> m vals (map f) (zipmap (keys m))))
 
 (defn some-let [f s]
   (some #(when (f %) %) s))
@@ -20,27 +23,36 @@
       (find-in child ks))
     :else v))
 
-(defn _get-select [v alias]
+(defn _get-cols [v alias select?]
   (when (vector? v)
     (case (v 0)
       "TOK_SELEXPR"
       (let-v [_ v [alias]]
-             (_get-select v alias))
-      "." [{:col (get-in v [2 0])
-            :table (get-in v [1 1 0])
-            :alias alias}]
-      "TOK_TABLE_OR_COL" [{:col (get-in v [1 0])
-                           :alias alias}]
-      "TOK_ALLCOLREF" [{:col "*"
-                        :table (get-in v [1 1 0])
-                        :alias alias}]
+             (_get-cols v alias true))
+      "."
+      (when select?
+        [{:col (get-in v [2 0])
+          :table (get-in v [1 1 0])
+          :alias alias}])
+      "TOK_TABLE_OR_COL"
+      (when select?
+        [{:col (get-in v [1 0])
+          :alias alias}])
+      "TOK_ALLCOLREF"
+      (when select?
+        [{:col "*"
+          :table (get-in v [1 1 0])
+          :alias alias}])
       "TOK_SUBQUERY" nil ;;don't go in there
-      (mapcat #(_get-select % alias) v))))
+      (mapcat #(_get-cols % alias select?) v))))
+
+(defn get-cols [v]
+  (->> (_get-cols v nil true) (map filter-v) distinct))
 
 (defn get-select [v]
-  (->> (_get-select v nil) (map filter-v) distinct))
+  (->> (_get-cols v nil false) (map filter-v) distinct))
 
-(defn _get-tables [v]
+(defn get-tables [v]
   (when (vector? v)
     (case (v 0)
       "TOK_TABREF"
@@ -51,9 +63,6 @@
       (let-v [_ subquery [alias]]
              [{:table alias :alias alias}])
       (mapcat _get-tables v))))
-
-(defn get-tables [v]
-  (distinct (_get-tables v)))
 
 (declare get-full)
 
@@ -70,6 +79,7 @@
 
 (defn get-full [v]
   {:select (get-select v)
+   :cols (get-cols v)
    :tables (get-tables v)
    :subqueries (get-subqueries v)})
 
@@ -79,7 +89,6 @@
       "TOK_TABCOLLIST"
       (->> v rest (map #(get-in % [1 0])))
       (mapcat _get-schema v))))
-
 (defn get-schema [v]
   [(find-in v ["TOK_TABNAME" 1 0])
    (_get-schema v)])
@@ -89,9 +98,40 @@
               :when (= "TOK_CREATETABLE" type)]
           (get-schema v))))
 
+(defmacro cond-let [v cond1 res1 & rest]
+  (if rest
+    `(if-let [~v ~cond1]
+      ~res1
+      (cond-let ~v ~@rest))
+    `(when-let [~v ~cond1] ~res1)))
+
+(defn _expand-stars [select tables subqueries get-real-table regular-schema]
+  (for [{:keys [col alias table] :as select-info} select
+        table-alias (cond
+                      (not= "*" col) [nil] ;;placeholder
+                      table [table]
+                      :else (map :alias tables))
+        col (cond-let x
+                      (not= "*" col) [select-info]
+                      (subqueries table-alias)
+                      (for [{:keys [col alias]} (:select x)]
+                        {:col (or alias col) :table table-alias})
+                      (-> table-alias get-real-table regular-schema)
+                      (for [col x]
+                        {:col col :table table-alias})
+                      :else (throw (Exception. (str "no table for " table-alias))))]
+    col))
+
+(defn expand-stars [{:keys [select subqueries tables] :as m} regular-schema]
+  (let [get-real-table (zipmap-by :alias :table tables)
+        subqueries (value-map #(expand-stars % regular-schema) subqueries)]
+    (-> m
+        (assoc :subqueries subqueries)
+        (update :select _expand-stars tables subqueries get-real-table regular-schema)
+        (update :cols _expand-stars tables subqueries get-real-table regular-schema))))
+
 (use 'clojure.pprint)
-(-> "a.hql"
-    slurp
-    (parse/parse-all parse/m)
-    get-schemas
-    pprint)
+(let [parsed (-> "b.hql" slurp (parse/parse-all parse/m))
+      schemas (get-schemas parsed)
+      subquery (-> parsed last get-full)]
+  (pprint (expand-stars subquery schemas)))
